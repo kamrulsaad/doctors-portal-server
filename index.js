6f69ded5ff6d3f3ed8625f8d4f32a5b754571d1c
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-05-21 16:13:01 +0600

    Added email sending feature and booking API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import 'dotenv/config'
 import { MongoClient, ServerApiVersion } from 'mongodb'
 import jwt from 'jsonwebtoken'
+import nodemailer from 'nodemailer'
+import sgTransport from 'nodemailer-sendgrid-transport'
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -22,6 +24,44 @@ function verifyJWT(req, res, next) {
     })
 }
 
+const emailSenderOptions = {
+    auth: {
+        api_key: process.env.EMAIL_SENDER_KEY
+    }
+}
+
+const sgClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+
+function senAppointmentEmail(booking) {
+
+    const {patient, patientEmail, date, slot, treatment} = booking
+
+    const email = {
+        from: process.env.EMAIL_SENDER,
+        to: patientEmail,
+        subject: `Your Appointment for ${treatment} is booked on ${date} at ${slot}`,
+        text: `Your Appointment for ${treatment} is booked on ${date} at ${slot}`,
+        html: `
+        <h2>Hello ${patient}</h2>
+        <p>You have recieved this email for booking a slot for ${treatment}.</p>
+        <p>The booking is fixed on ${date} at ${slot}</p>
+        <p>We're highlt delighted to have you for providing you the world's best medical treatment service. Hope to see you soon.</p>
+        <p>Any Issues, Contact us: +880123456789</p>
+        <a href='https://marvelous-toy-store.web.app/'>Follow us</a>
+        `
+    };
+
+    sgClient.sendMail(email, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Message sent: ' + info);
+        }
+    });
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal.yfumd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
@@ -32,12 +72,24 @@ async function run() {
         const servicesCollection = client.db("doctors_portal").collection("services")
         const bookingCollection = client.db("doctors_portal").collection("booking")
         const usersCollection = client.db("doctors_portal").collection("users")
+        const doctorsCollection = client.db("doctors_portal").collection("doctors")
+
+        // verify Admin middleware
+
+        async function verfyAdmin(req, res, next) {
+            const requester = req.decoded.email
+            const requestedAccount = await usersCollection.findOne({ email: requester })
+            if (requestedAccount.role === 'Admin') {
+                next()
+            }
+            else res.status(403).send({ message: "Forbidden Access" })
+        }
 
         // all services API 
 
         app.get('/services', async (req, res) => {
             const query = {}
-            const cursor = servicesCollection.find(query)
+            const cursor = servicesCollection.find(query).project({ name: 1 })
             const result = await cursor.toArray()
             res.send(result)
         })
@@ -50,6 +102,7 @@ async function run() {
             const exists = await bookingCollection.findOne(query)
             if (exists) return res.send({ success: false, booking: exists })
             const result = await bookingCollection.insertOne(booking)
+            senAppointmentEmail(booking)
             res.send({ success: true, result })
         })
 
@@ -61,7 +114,7 @@ async function run() {
                 const result = await bookingCollection.find(query).toArray()
                 res.send(result)
             }
-            else return res.status(403).send({message: 'Forbidden Access'})
+            else return res.status(403).send({ message: 'Forbidden Access' })
         })
 
         // availbale appointments API 
@@ -83,13 +136,13 @@ async function run() {
 
         // users API
 
-        app.get('/users', verifyJWT, async(req, res) =>{
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
 
-        app.delete('/users', verifyJWT, async(req, res) =>{
-            const query = {email: req.query.email}
+        app.delete('/users', verifyJWT, async (req, res) => {
+            const query = { email: req.query.email }
             const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
@@ -107,26 +160,41 @@ async function run() {
             res.send({ result, token })
         })
 
-        app.get('/admin/:email',verifyJWT, async(req, res) => {
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email
-            const user = await usersCollection.findOne({email})
+            const user = await usersCollection.findOne({ email })
             const isAdmin = user.role === "Admin"
-            res.send({admin: isAdmin})
+            res.send({ admin: isAdmin })
         })
 
-        app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verfyAdmin, async (req, res) => {
             const email = req.params.email
-            const requester = req.decoded.email
-            const requestedAccount = await usersCollection.findOne({email: requester})
-            if(requestedAccount.role === 'Admin'){
-                const filter = { email: email }
-                const updateDoc = {
-                    $set: {role: "Admin"}
-                }
-                const result = await usersCollection.updateOne(filter, updateDoc)
-                res.send(result)
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: "Admin" }
             }
-            else res.status(403).send({message: "Forbidden Access"})
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+
+        })
+
+        // doctors API 
+
+        app.get('/doctors', verifyJWT, verfyAdmin, async (req, res) => {
+            const result = await doctorsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/doctors', verifyJWT, verfyAdmin, async (req, res) => {
+            const doctor = req.body
+            const result = await doctorsCollection.insertOne(doctor)
+            res.send(result)
+        })
+
+        app.delete('/doctor/:email', verifyJWT, verfyAdmin, async (req, res) => {
+            const email = req.params.email
+            const result = await doctorsCollection.deleteOne({ email })
+            res.send(result)
         })
 
 
